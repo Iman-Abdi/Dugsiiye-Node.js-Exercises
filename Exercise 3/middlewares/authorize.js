eea95e38131ef4fabc377c9b4cce6e0f7a592916
Iman-Abdi <leofaragaab1@gmail.com>
2026-06-15 18:10:47 +0300

    Exercise 3

---
+++ Exercise 3/middlewares/authorize.js
@@ -0,0 +1,17 @@
+const authorize =
+  (...roles) =>
+  (req, res, next) => {
+    if (
+      !roles.includes(req.user.role)
+    ) {
+      return res.status(403).json({
+        message: `Access denied: Requires one of [${roles.join(
+          ", "
+        )}]`,
+      });
+    }
+
+    next();
+  };
+
+export default authorize;
